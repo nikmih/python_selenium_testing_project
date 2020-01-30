2e84b4633f9da54a96b8ef31a9209a9319255d7f
Nikita Mikhailov <nikmih87@gmail.com>
2020-01-30 20:40:10 +0100

    locators file added, new locators added to MainPage class

--- pages/base_page.py
+++ pages/base_page.py
@@ -2,7 +2,7 @@ from selenium.common.exceptions import NoSuchElementException
 from selenium.webdriver.common.by import By
 
 class BasePage():
-    def __init__(self, browser, url, timeout=5):
+    def __init__(self, browser, url, timeout=10):
         self.browser = browser
         self.url = url
         self.browser.implicitly_wait(timeout)
